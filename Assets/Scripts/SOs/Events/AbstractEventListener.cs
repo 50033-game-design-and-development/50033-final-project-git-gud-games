f29617c782e9f090142222b82c63cfecdb69a96b
Divy <divy1211.dc@gmail.com>
2023-11-05 15:02:27 +0800

    Added Event static class and SO GameEvents

---
+++ Assets/Scripts/SOs/Events/AbstractEventListener.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class AbstractEventListener<T> : MonoBehaviour {
+    public AbstractEvent<T> @event;
+    public UnityEvent<T> response;
+
+    public void OnRaised(T data) {
+        response.Invoke(data);
+    }
+    private void OnEnable() {
+        @event.AddListener(this);
+    }
+    private void OnDisable() {
+        @event.RemoveListener(this);
+    }
+}
+
+public class AbstractEventListener : MonoBehaviour {
+    public AbstractEvent @event;
+    public UnityEvent response;
+
+    public void OnRaised() {
+        response.Invoke();
+    }
+    private void OnEnable() {
+        @event.AddListener(this);
+    }
+    private void OnDisable() {
+        @event.RemoveListener(this);
+    }
+}
