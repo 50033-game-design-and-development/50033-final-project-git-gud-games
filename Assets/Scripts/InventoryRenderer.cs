26f007d4a1ef4483ae1c89ae26cf097e3d899407
milselarch <milselarch@gmail.com>
2023-11-05 15:45:42 +0800

    overhual hotbar implementation using UI toolkit flexbox

--- Assets/Scripts/InventoryRenderer.cs
+++ Assets/Scripts/InventoryRenderer.cs
@@ -2,42 +2,35 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
+using UnityEngine.Assertions;
+using UnityEngine.UIElements;
 
 public class InventoryRenderer: MonoBehaviour {
-    public Image hotbarBackground;
+    // public Image hotbarBackground;
+    public UIDocument hotbarUI;
     public GameObject hotbarPrefab;
 
-    public int padding = 20;
+    private float _padding;
     // gap between bar item images
     public int hotbarItemGap = 40;
     private List<GameObject> _hotbarItems = new List<GameObject>();
-    
-    // Start is called before the first frame update
-    void Start() {
-        var rectTransform = hotbarBackground.rectTransform;
-        var imageRect = rectTransform.rect;
-        var width = imageRect.width;
-        var height = imageRect.height;
-        var hotbarLeftmostX = (
-            rectTransform.anchoredPosition.x - 
-            rectTransform.rect.width * rectTransform.pivot.x
-        );
-        Vector2 hotbarScreenLeftmostX = RectTransformUtility.WorldToScreenPoint(
-            null, rectTransform.TransformPoint(
-                new Vector3(hotbarLeftmostX, 0f, 0f)
-            )
-        );
 
-        // Log the dimensions
-        Debug.Log($"Image Dimensions: Width = {width}, Height = {height}");
-        var usableWidth = width - padding * 2;
-        var hotbarItemSize = height - padding * 2;
+    public int CalculateNumberOfHotbarItems(
+        RectTransform rectTransform, float hotbarItemSize
+    ) {
+        VisualElement root = hotbarUI.rootVisualElement;
+        VisualElement hotbarElement = root.Q("Hotbar");
+        _padding = hotbarElement.resolvedStyle.paddingLeft;
+
+        var imageRect = rectTransform.rect;
+        var hotbarWidth = imageRect.width;
+        var usableWidth = hotbarWidth - _padding * 2;
         /*
         n - number of hotbar items
         H - hotbar item size
+        u - usable width (width - left and right  padding)
         g - gap size
-    
+
         (n-1)*g + n*H = u
         n*g - g + n*H = u
         n*(g+H) - g = u
@@ -46,29 +39,50 @@ public class InventoryRenderer: MonoBehaviour {
         var numHotbarItems = (int) Math.Floor(
             (usableWidth + hotbarItemGap) / (hotbarItemSize + hotbarItemGap)
         );
-        var remainingSpace = (float) Math.Floor(
-            usableWidth - numHotbarItems * hotbarItemSize - 
-            (numHotbarItems - 1) * hotbarItemGap
-        );
+        return numHotbarItems;
+    }
+    
+    private IEnumerator PopulateInventory() {
+        // wait for UI document to render
+        yield return null;
+        var rectTransform = hotbarUI.GetComponent<RectTransform>();
         
-        Debug.Log("REM_SPACE " + remainingSpace);
-        for (var k = 0; k < numHotbarItems; k++) {
-            // calculate position of the hotbar item relative
-            // to the center of the hotbar background
-            var itemPosition = new Vector3(
-                (k + 0.5f) * hotbarItemSize + hotbarItemGap * k -
-                width / 2.0f + hotbarScreenLeftmostX.x,
-                0.0f, 0.0f
-            );
-            GameObject instance = Instantiate(
-                hotbarPrefab, Vector3.zero, Quaternion.identity,
-                hotbarBackground.transform
-            );
-            instance.transform.localPosition = itemPosition;
-            instance.GetComponent<RectTransform>().sizeDelta = new Vector2(
-                hotbarItemSize, hotbarItemSize
-            );
+        VisualElement root = hotbarUI.rootVisualElement;
+        VisualElement hotbarElement = root.Q<VisualElement>("Hotbar");
+        _padding = (
+            hotbarElement.resolvedStyle.paddingLeft +
+            hotbarElement.resolvedStyle.paddingRight
+        ) / 2.0f;
+        
+        var imageRect = rectTransform.rect;
+        var hotbarHeight = imageRect.height;
+        Debug.Log("HOTBAR_HEIGHT " + hotbarHeight);
+        var hotbarItemHeight = hotbarHeight - _padding * 2;
+        Debug.Log("HOTBAR_ITEM_HEIGHT " + hotbarItemHeight);
+        var numHotbarItems = CalculateNumberOfHotbarItems(
+            rectTransform, hotbarItemHeight
+        );
+
+        for (int k = 0; k < numHotbarItems; k++) {
+            // Create a new button.
+            Button inventoryItem = new Button();
+            inventoryItem.style.width = hotbarItemHeight;
+            hotbarElement.Add(inventoryItem);
         }
+        
+        yield return null;
+    }
+    
+    // Start is called before the first frame update
+    void Start() {
+        VisualElement root = hotbarUI.rootVisualElement;
+        var buttons = root.Query<Button>().ToList();
+        // Remove pre-existing buttons from the inventory 
+        foreach (var button in buttons) {
+            button.RemoveFromHierarchy();
+        }
+        
+        StartCoroutine(PopulateInventory());
     }
 
     // Update is called once per frame
