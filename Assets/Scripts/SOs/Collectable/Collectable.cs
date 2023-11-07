477730ac4121c16c010800cba494542744096a11
Divy <divy1211.dc@gmail.com>
2023-11-07 09:33:49 +0800

    Moved Collectable into Scripts

--- Assets/Scripts/SOs/Collectable/Collectable.cs
+++ Assets/Scripts/SOs/Collectable/Collectable.cs
@@ -2,10 +2,11 @@ using UnityEngine;
 
 // Create a menu item for creating the ScriptableObject asset
 [CreateAssetMenu(
-    fileName = "Collectable", 
-    menuName = "Inventory/Collectable", order = 1
+    fileName = "Collectable",
+    menuName = "Inventory/Collectable",
+    order = 1
 )]
-public class Collectable: ScriptableObject {
+public class Collectable : ScriptableObject {
     public InventoryItems itemType; // The enum member
     public Sprite itemSprite; // The sprite member
 }
