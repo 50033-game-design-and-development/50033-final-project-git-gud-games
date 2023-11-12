f026d828d463622acfd2d7ff5d93af3cfd0d6fc5
milselarch <milselarch@gmail.com>
2023-11-12 15:39:52 +0800

    set highlight based on mouse position when inventory is opened

--- Assets/Scripts/Player/PlayerRayCast.cs
+++ Assets/Scripts/Player/PlayerRayCast.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 
 public class PlayerRayCast : MonoBehaviour {
+    private static Color OUTLINE_COLOR = new Color(255, 255, 255, 0.8f);
     public PlayerConstants playerConstants;
 
     private Transform _highlight;
@@ -9,11 +10,14 @@ public class PlayerRayCast : MonoBehaviour {
     private readonly Vector3 _rayOrigin = new(0.5f, 0.5f, 0f);
     private int _layerMaskInteractable;
 
+    private void PerformHighlight(Transform transform) {
+        PerformHighlight(transform, OUTLINE_COLOR);
+    }
 
     /// <summary>
     /// Performs highlighting of a valid interactable object
     /// </summary>
-    private void PerformHighlight(Transform transform) {
+    private void PerformHighlight(Transform transform, Color highlightColor) {
         // Hover over same object. Do nothing
         if (_highlight != null && _highlight.GetInstanceID() == transform.GetInstanceID())
             return;
@@ -24,23 +28,28 @@ public class PlayerRayCast : MonoBehaviour {
         _highlighted = true;
         _highlight = transform;
 
-        EnableOutline();
+        EnableOutline(highlightColor);
     }
-
+    
     private void EnableOutline() {
         if (_highlight == null)
             return;
-        EnableOutline(_highlight.gameObject);
+        EnableOutline(_highlight.gameObject, OUTLINE_COLOR);
+    }
+    
+    private void EnableOutline(Color highlightColor) {
+        EnableOutline(_highlight.gameObject, highlightColor);       
     }
 
-    private void EnableOutline(GameObject obj) {
+    private void EnableOutline(GameObject obj, Color highlightColor) {
         if (obj == null)
             return;
         if (obj.GetComponent<Outline>() != null) {
+            obj.GetComponent<Outline>().OutlineColor = highlightColor;
             obj.GetComponent<Outline>().enabled = true;
         } else {
             Outline outline = obj.AddComponent<Outline>();
-            outline.OutlineColor = new Color(255, 255, 255, 0.8f);
+            outline.OutlineColor = highlightColor;
             outline.OutlineMode = Outline.Mode.OutlineVisible;
             outline.OutlineWidth = 5f;
             outline.enabled = true;
@@ -80,16 +89,43 @@ public class PlayerRayCast : MonoBehaviour {
 
     private void Update() {
         // Handle cases where player hovers away from object
-        if (_highlight != null && !_highlighted)
+        if (_highlight != null && !_highlighted) {
             DisableOutline();
-
-        // Ray points out from the middle of camera viewport 
-        Ray ray = Camera.main.ViewportPointToRay(_rayOrigin);
-        if (Physics.Raycast(ray, out RaycastHit raycastHit, playerConstants.raycastDistance, _layerMaskInteractable)) {
-            PerformHighlight(raycastHit.transform);
-            return;
         }
 
+        if (GameState.InventoryOpened) {
+            if (!GameState.IsDraggingInventoryItem) {
+                _highlighted = false;
+                DisableOutline();
+                return;
+            }
+            
+            // Ray points out from cursor position in camera viewport
+            Ray ray = Camera.main.ScreenPointToRay(
+                GameState.LastPointerDragScreenPos
+            );
+            
+            if (Physics.Raycast(
+                ray, out RaycastHit raycastHit, playerConstants.raycastDistance,
+                _layerMaskInteractable
+            )) {
+                PerformHighlight(raycastHit.transform);
+                return;
+            }
+        } else {
+            // Ray points out from the middle of camera viewport 
+            Ray ray = Camera.main.ViewportPointToRay(_rayOrigin);
+            if (Physics.Raycast(
+                ray, out RaycastHit raycastHit, playerConstants.raycastDistance,
+                _layerMaskInteractable
+            )) {
+                PerformHighlight(raycastHit.transform);
+                return;
+            }
+        }
+        
+        
         _highlighted = false;
+        DisableOutline();
     }
 }
