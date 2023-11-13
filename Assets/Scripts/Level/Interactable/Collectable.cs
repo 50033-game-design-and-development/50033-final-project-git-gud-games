a443816fefe5bfce30e0b4d58c70de63ec51a423
keee99 <kohjj99@gmail.com>
2023-11-14 01:45:20 +0800

    Implement paper going into inventory

--- Assets/Scripts/Level/Interactable/Collectable.cs
+++ Assets/Scripts/Level/Interactable/Collectable.cs
@@ -1,12 +1,25 @@
 using UnityEngine;
 
 public class Collectable : MonoBehaviour, IInteractable {
-    public int itemIndex;
-    // public IntGameEvent onItemCollected;
+
+    public GameEvent onInventoryUpdate;
+    public Sprite invSprite;
+    public InventoryItems itemType;
+
+    private Inv.Collectable invItem {
+        get => new Inv.Collectable {
+            itemType = itemType,
+            itemSprite = invSprite,
+        };
+    }
 
     // TODO:
     public void OnInteraction() {
         Debug.Log("Collect " + gameObject.name);
+
+        GameState.inventory.Add(invItem);
+        onInventoryUpdate.Raise();
         Destroy(gameObject);
+        
     }
 }
