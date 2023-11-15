607d9bf445a4cecade015d7642a8af8aad97f2f1
jaron <jaronho@protonmail.com>
2023-11-15 19:16:43 +0800

    Removed AudioMap and StringMap

--- Assets/Scripts/SOs/locals/AudioMap.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.Audio;
-
-[CreateAssetMenu(fileName = "AudioMap", menuName = "ScriptableObjects/locals/AudioMap", order = 2)]
-public class AudioMap : ScriptableObject {
-    public List<AudioClip> audioList;
-}
--- Assets/Scripts/SOs/locals/StringMap.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "StringMap", menuName = "ScriptableObjects/locals/StringMap", order = 1)]
-public class StringMap : ScriptableObject {
-    public List<string> textList;
-}
