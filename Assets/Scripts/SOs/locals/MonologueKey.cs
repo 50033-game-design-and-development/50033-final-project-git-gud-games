61b7d8b364748f6e98105810138b77320d90aa80
jaron <jaronho@protonmail.com>
2023-11-16 23:54:57 +0800

    Restored int assignment to MonologueKey

--- Assets/Scripts/SOs/locals/MonologueKey.cs
+++ Assets/Scripts/SOs/locals/MonologueKey.cs
@@ -4,16 +4,16 @@ using UnityEngine;
 
 // Manually assigned numbers to prevent breakage
 public enum MonologueKey {
-    NULL,
-    L0_START,
-    L0_PAPER_TABLE,
-    L0_PAPER_WARDROBE,
-    L0_DOOR_LOCKED,
-    L0_CORPSE_DEFAULT,
-    L0_CORPSE_AFTER_P1,
-    L0_CORPSE_AFTER_KEY,
-    L0_BOX,
-    L0_BARREL,
-    L0_PAPER_TABLE_AFTER_P1,
-    L0_PAPER_TABLE_AFTER_KEY,
+    NULL = 0,
+    L0_START = 1,
+    L0_PAPER_TABLE = 2,
+    L0_PAPER_WARDROBE = 3,
+    L0_DOOR_LOCKED = 4,
+    L0_CORPSE_DEFAULT = 5,
+    L0_CORPSE_AFTER_P1 = 6,
+    L0_CORPSE_AFTER_KEY = 7,
+    L0_BOX = 8,
+    L0_BARREL = 9,
+    L0_PAPER_TABLE_AFTER_P1 = 10,
+    L0_PAPER_TABLE_AFTER_KEY = 11,
 }
