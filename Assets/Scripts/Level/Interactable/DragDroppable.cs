45a4e8e10f38350cfea6971e270930c7acdb06d0
Divy <divy1211.dc@gmail.com>
2023-11-17 18:33:36 +0800

    Added support for click, and drag & drop interactions when focused

---
+++ Assets/Scripts/Level/Interactable/DragDroppable.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragDoppable : MonoBehaviour, IDragDroppable {
+    public List<InventoryItem> possibleDroppable;
+    public GameEvent @event;
+
+    private HashSet<InventoryItem> _possibleDroppable;
+    public void OnDragDrop() {
+        if (GameState.selectedInventoryItem == null) {
+            return;
+        }
+        var selectedInventoryItem = GameState.selectedInventoryItem.Value;
+        if (!_possibleDroppable.Contains(selectedInventoryItem.itemType)) {
+            return;
+        }
+        GameState.inventory.Remove(selectedInventoryItem);
+
+        @event.Raise();
+    }
+    private void Awake() {
+        _possibleDroppable = new(possibleDroppable);
+    }
+}
