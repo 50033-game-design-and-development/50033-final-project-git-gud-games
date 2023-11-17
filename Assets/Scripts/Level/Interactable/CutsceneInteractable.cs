d60bbe0b94ce3a1db9e55a2d941346d106227a1c
keee99 <kohjj99@gmail.com>
2023-11-18 02:58:44 +0800

    Reformat

--- Assets/Scripts/Level/Interactable/CutsceneInteractable.cs
+++ Assets/Scripts/Level/Interactable/CutsceneInteractable.cs
@@ -1,34 +1,29 @@
-
 using UnityEngine;
 using UnityEngine.Playables;
+
 public class CutsceneInteractable : MonoBehaviour, IInteractable {
     private int state = 0;
-    
-    [SerializeField]
-    private int playState = 1;
+
+    [SerializeField] private int playState = 1;
 
     public void OnInteraction() {
         if (state == playState) {
-            PlayCutscene();    
+            PlayCutscene();
             IncrementState();
         }
-              
     }
 
     private void PlayCutscene() {
         // TODO: Lock player input
-        
+
         PlayableDirector director = GetComponent<PlayableDirector>();
         director.Play();
-        
+
         // while (director.state == PlayState.Playing);
         // TODO: Unlock player input
-
     }
 
     public void IncrementState() {
         state++;
     }
-
-
 }
