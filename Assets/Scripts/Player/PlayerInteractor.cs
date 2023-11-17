12adf4b41886d1e65db3e58b6d1e4cbb108c94ae
Divy <divy1211.dc@gmail.com>
2023-11-17 19:30:41 +0800

    Fixed player interactor

--- Assets/Scripts/Player/PlayerInteractor.cs
+++ Assets/Scripts/Player/PlayerInteractor.cs
@@ -1,9 +1,13 @@
+using Cinemachine;
 using UnityEngine;
 
 public class PlayerInteractor : MonoBehaviour {
     private PlayerAction _playerAction;
     private int _layerMaskInteractable;
-    
+
+    public GameEvent onInventoryUpdate;
+    public CinemachineStateDrivenCamera cineMachineCamera;
+    public CinemachineVirtualCamera firstPersonCamera;
     public PlayerConstants playerConstants;
 
     private void TriggerInteractions(Vector2 screenPos) {
@@ -15,7 +19,7 @@ public class PlayerInteractor : MonoBehaviour {
     }
 
     private static void Interact(GameObject obj) {
-        if (!GameState.inventoryOpened) {
+        if(!GameState.inventoryOpened) {
             foreach (var i in obj.GetComponents<IInteractable>()) {
                 i.OnInteraction();
             }
@@ -31,7 +35,7 @@ public class PlayerInteractor : MonoBehaviour {
             i.OnClick();
         }
     }
-    
+
     private void Start() {
         _layerMaskInteractable = LayerMask.GetMask("Interactable");
 
@@ -41,10 +45,11 @@ public class PlayerInteractor : MonoBehaviour {
             GameState.lastPointerDragScreenPos = ctx.ReadValue<Vector2>();
         };
 
-        // trigger interaction with object on click
         // and when inventory is not opened
+        // trigger interaction with object on click
         _playerAction.gameplay.MousePress.performed += ctx => {
             TriggerInteractions(GameState.lastPointerDragScreenPos);
+            GameState.mouseHold = true;
         };
 
         // trigger drag interaction with object on mouse release
@@ -53,9 +58,37 @@ public class PlayerInteractor : MonoBehaviour {
             if (GameState.isDraggingInventoryItem) {
                 TriggerInteractions(GameState.lastPointerDragScreenPos);
             }
-            
+
             GameState.isDraggingInventoryItem = false;
             GameState.selectedInventoryItem = null;
+            GameState.mouseHold = false;
+        };
+
+        // open inventory when you press E
+        _playerAction.gameplay.InventoryOpen.performed += _ => {
+            Debug.Log("TOGGLE");
+            GameState.ToggleInventory();
+
+            // check if the cinemachine camera is not locked
+            // to any interaction objects i.e. it follows the player
+            if (cineMachineCamera.LiveChild == firstPersonCamera) {
+                if (GameState.inventoryOpened) {
+                    // disable the cineMachineCamera if the inventory
+                    // is opened, otherwise the camera will follow
+                    // the cursor position
+                    cineMachineCamera.enabled = false;
+                } else {
+                    cineMachineCamera.enabled = true;
+                }
+            }
+
+            onInventoryUpdate.Raise();
+        };
+
+        // close inventory when you press escape
+        _playerAction.gameplay.Escape.performed += _ => {
+            // GameState.HideInventory();
+            onInventoryUpdate.Raise();
         };
     }
 }
