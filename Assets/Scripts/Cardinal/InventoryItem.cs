de878cb587b03092ffdfaf3a390a63b2dea39359
keee99 <kohjj99@gmail.com>
2023-11-18 16:10:48 +0800

    Prepare collectable scripts

--- Assets/Scripts/Cardinal/InventoryItem.cs
+++ Assets/Scripts/Cardinal/InventoryItem.cs
@@ -2,4 +2,14 @@
     Ball,
     Paper,
     Key,
+
+    L1Beer,
+    L1Garlic,
+    L1Lemon,
+    L1Onion,
+    L1Mushroom,
+    L1Chicken,
+    L1Carrot,
+    L1Tomato,
+    L1Pencil,
 }
--- Assets/Scripts/Level/Interactable/Collectable.cs
+++ Assets/Scripts/Level/Interactable/Collectable.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using UnityEngine;
 
 public class Collectable : MonoBehaviour, IInteractable {
-    public GameEvent onInventoryUpdate;
     public Sprite invSprite;
     public InventoryItem itemType;
 
@@ -14,7 +13,7 @@ public class Collectable : MonoBehaviour, IInteractable {
     public void OnInteraction() {
         Debug.Log("Collect " + gameObject.name);
         GameState.inventory.Add(invItem);
-        onInventoryUpdate.Raise();
+        Event.onInventoryUpdate.Raise();
         float duration = GetComponent<SFXInteractable>().audioClips[0].length;
         StartCoroutine("Collect", duration);
     }
