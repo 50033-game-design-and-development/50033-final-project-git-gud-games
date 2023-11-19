bf9821ad56ad1cd363df5e078a7d794970be8a7e
keee99 <kohjj99@gmail.com>
2023-11-19 23:32:43 +0800

    Implement placing food into pot

--- Assets/Scripts/Level/Puzzles/L1/P2Pot.cs
+++ Assets/Scripts/Level/Puzzles/L1/P2Pot.cs
@@ -7,22 +7,15 @@ public class P2Pot : MonoBehaviour {
     [Header("Debug")]
     public List<InventoryItem> potItems = new List<InventoryItem>();
 
-    [Header("Item details")]    
+    [Header("Attributes")]
+    [SerializeField] private Transform ingredientsTransform;
+    
+    [Header("Ingredient details")]    
     [SerializeField] private List<InventoryItem> validItems = new List<InventoryItem>();
     [SerializeField] private List<GameObject> itemPrefabs = new List<GameObject>();
     
 
-    // public Transform ingredientTransform;
-
-    // Start is called before the first frame update
-    void Start() {
-
-    }
-
-    // Update is called once per frame
-    void Update() {
-        
-    }
+    
 
     public void OnDragDrop() {
         if (!GameState.selectedInventoryItem.HasValue) 
@@ -41,7 +34,10 @@ public class P2Pot : MonoBehaviour {
     public void InstantiatePrefab(int index) {
         GameObject prefab = itemPrefabs[index];
 
-        Debug.LogWarning("Unimplemented");
+        // Spawn prefab at ingredientTransform as child of this
+        GameObject ingredient = Instantiate(prefab, ingredientsTransform);
+        ingredient.transform.parent = ingredientsTransform;
+        
     }
 
 
