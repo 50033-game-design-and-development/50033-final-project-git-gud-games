0e4b42741c360702c96d4ef6ab2c71c47a755177
Divy <divy1211.dc@gmail.com>
2023-11-19 14:25:55 +0800

    Fixed private modifier

--- Assets/Scripts/Cardinal/Global.cs
+++ Assets/Scripts/Cardinal/Global.cs
@@ -3,18 +3,18 @@ using UnityEngine;
 namespace Event {
     public class Global : MonoBehaviour {
         public static GameEvent hideAll;
+        public static GameEvent inventoryUpdate;
         public static GameEvent revealAll;
         public static Vector3GameEvent revealPositionOnUi;
         public static MonologueKeyGameEvent showDialogue;
-        public static GameEvent inventoryUpdate;
 
         public GameEvent _hideAll;
+        public GameEvent _inventoryUpdate;
         public GameEvent _revealAll;
         public Vector3GameEvent _revealPositionOnUi;
         public MonologueKeyGameEvent _showDialogue;
-        public GameEvent _inventoryUpdate;
 
-        void Start() {
+        private void Start() {
             revealAll = _revealAll;
             hideAll = _hideAll;
             revealPositionOnUi = _revealPositionOnUi;
