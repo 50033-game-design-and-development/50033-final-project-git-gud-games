ed31be3cf9e314b2d4b49f78dbb5e9e7285ea3f6
Eliana Setiabudi <amidis100@gmail.com>
2023-11-19 21:40:23 +0800

    Imported Fusebox Prefab

---
+++ Assets/Scene Assets/FuseBox/Scripts/Managers/FBInventoryManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace FuseboxSystem
+{
+    public class FBInventoryManager : MonoBehaviour
+    {
+        [Header("Fuses in Inventory")]
+        public int inventoryFuses;
+
+        public static FBInventoryManager instance;
+
+        private void Awake()
+        {
+            if (instance != null) { Destroy(gameObject); }
+            else { instance = this; DontDestroyOnLoad(gameObject); }
+        }
+
+        public void AddFuse()
+        {
+            inventoryFuses++;
+            FBUIManager.instance.UpdateFuseUI(inventoryFuses);
+        }
+
+        public void RemoveFuse()
+        {
+            inventoryFuses--;
+            FBUIManager.instance.UpdateFuseUI(inventoryFuses);
+        }
+    }
+}
