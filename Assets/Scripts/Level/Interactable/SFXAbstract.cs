d44788f2caf92173a8c31b3cee1528639b1615a0
jaron <jaronho@protonmail.com>
2023-11-21 18:58:11 +0800

    Fixed SFXAbstract GetAudioLength bug

--- Assets/Scripts/Level/Interactable/SFXAbstract.cs
+++ Assets/Scripts/Level/Interactable/SFXAbstract.cs
@@ -15,7 +15,7 @@ public abstract class SFXAbstract : MonoBehaviour {
 
     public float GetAudioLength() {
         if (cachedState == -1) {
-            return 0;
+            return audioClips[0].length;
         }
         return audioClips[cachedState].length;
     }
