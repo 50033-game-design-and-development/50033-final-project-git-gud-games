38d2d34c747ab19b2b1414732cd7a2a4c522cbaa
Divy <divy1211.dc@gmail.com>
2023-11-23 16:56:49 +0800

    Fixed double interactions with keypad on scene transition and no interactions on scene transition

--- Assets/Scripts/UI/NextScene.cs
+++ Assets/Scripts/UI/NextScene.cs
@@ -8,12 +8,11 @@ public class NextScene : MonoBehaviour {
 
     public void LoadSingle() {
         GameState.isInventoryOpened = false;
+        GameState.isPuzzleLocked = false;
         SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Single);
     }
 
     public void LoadAdditive() {
         SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Additive);
     }
-
-
 }
