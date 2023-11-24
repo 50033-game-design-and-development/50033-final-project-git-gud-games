ef5bb19143cf715c5db705024bc75eaea6f4c385
Divy <divy1211.dc@gmail.com>
2023-11-24 22:17:20 +0800

    Used guard clause in OnNotePlayed

--- Assets/Scripts/Level/Puzzles/L2/P4NoteSequence.cs
+++ Assets/Scripts/Level/Puzzles/L2/P4NoteSequence.cs
@@ -10,15 +10,16 @@ public class P4NoteSequence : MonoBehaviour {
     public void OnNotePlayed(Music.Note note, Music.Octave octave) {
         if (note != sequence[_idx]) {
             _idx = 0;
-            Debug.Log("idx: "+_idx);
             return;
         }
+
         ++_idx;
-        Debug.Log("idx: "+_idx);
-        if (_idx == sequence.Count) {
-            Event.L2.solvedP4.Raise();
-            Debug.Log("solved");
-            enabled = false;
+        if (_idx != sequence.Count) {
+            return;
         }
+
+        Event.L2.solvedP4.Raise();
+        Debug.Log("solved");
+        enabled = false;
     }
 }
