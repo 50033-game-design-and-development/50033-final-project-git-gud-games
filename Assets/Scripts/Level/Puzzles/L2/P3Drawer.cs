f430ecbaa8550c3c9841b862645134fb9d31a92a
Divy <divy1211.dc@gmail.com>
2023-11-25 00:44:08 +0800

    Made floppy disk obtainable

---
+++ Assets/Scripts/Level/Puzzles/L2/P3Drawer.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class P3Drawer : MonoBehaviour {
+    public Animator animator;
+    private static readonly int INTERACT = Animator.StringToHash("Interact");
+
+    // called on L2::SolvedP3
+    public void OnPuzzleSolved() {
+        animator.SetTrigger(INTERACT);
+    }
+}
