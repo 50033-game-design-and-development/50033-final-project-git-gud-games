07a3ce8306f51807701b25b67951ac69df9b5f20
Divy <divy1211.dc@gmail.com>
2023-11-24 23:24:20 +0800

    Using game event in keypad

--- Assets/Scripts/Cardinal/Event/L1.cs
+++ Assets/Scripts/Cardinal/Event/L1.cs
@@ -2,24 +2,24 @@
 
 namespace Event {
     public class L1 : MonoBehaviour {
+        public static GameEvent checkStew;
+        public static GameEvent drinkStew;
         public static GameEvent placeIngredient;
         public static InventoryItemGameEvent removeIngredient;
-        public static GameEvent solveP2;
         public static GameEvent seeBook;
         public static GameEvent solveP1;
-        public static GameEvent drinkStew;
+        public static GameEvent solveP2;
         public static GameEvent unlockDoor;
-        public static GameEvent checkStew;
 
         [SerializeField] private GameEvent _checkStew;
         [SerializeField] private  GameEvent _drinkStew;
-        [SerializeField] private  GameEvent _L1unlockDoor;
         [SerializeField] private GameEvent _placeIngredient;
         [SerializeField] private InventoryItemGameEvent _removeIngredient;
         [SerializeField] private  GameEvent _seeBook;
         [SerializeField] private  GameEvent _solveP1;
         [SerializeField] private GameEvent _solveP2;
-        
+        [SerializeField] private  GameEvent _unlockDoor;
+
         private void Start() {
             placeIngredient = _placeIngredient;
             removeIngredient = _removeIngredient;
@@ -28,7 +28,7 @@ namespace Event {
             solveP1 = _solveP1;
             solveP2 = _solveP2;
             drinkStew = _drinkStew;
-            unlockDoor = _L1unlockDoor;
+            unlockDoor = _unlockDoor;
             checkStew = _checkStew;
         }
     }
--- Assets/Scripts/Cardinal/Event/L2.cs
+++ Assets/Scripts/Cardinal/Event/L2.cs
@@ -5,15 +5,18 @@ namespace Event {
         public static GameEvent clickPainting;
         public static NoteOctaveGameEvent playNote;
         public static GameEvent plugFuse;
+        public static GameEvent solveP3;
 
         [SerializeField] private GameEvent _clickPainting;
         [SerializeField] private NoteOctaveGameEvent _playNote;
         [SerializeField] private GameEvent _plugFuse;
+        [SerializeField] private GameEvent _solveP3;
 
         private void Start() {
             clickPainting = _clickPainting;
             playNote = _playNote;
             plugFuse = _plugFuse;
+            solveP3 = _solveP3;
         }
     }
 }
