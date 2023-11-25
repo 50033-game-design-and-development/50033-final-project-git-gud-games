25eceabd6520498e5d8247942f4650063710319c
Divy <divy1211.dc@gmail.com>
2023-11-25 11:56:47 +0800

    Made keypad and floppy tab highlightable

--- Assets/Scripts/Level/Puzzles/L2/P3Drawer.cs
+++ Assets/Scripts/Level/Puzzles/L2/P3Drawer.cs
@@ -2,10 +2,13 @@
 
 public class P3Drawer : MonoBehaviour {
     public Animator animator;
+    public Revealable floppy;
+
     private static readonly int INTERACT = Animator.StringToHash("Interact");
 
     // called on L2::SolvedP3
     public void OnPuzzleSolved() {
         animator.SetTrigger(INTERACT);
+        floppy.isVisible = true;
     }
 }
