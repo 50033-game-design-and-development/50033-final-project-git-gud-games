831d43735188bfce9db6cea5804f12be5e330352
jaron <jaronho@protonmail.com>
2023-11-26 15:00:29 +0800

    Restored unused events in cardinal

--- Assets/Scripts/Cardinal/Event/L0.cs
+++ Assets/Scripts/Cardinal/Event/L0.cs
@@ -2,16 +2,25 @@
 
 namespace Event {
     public class L0 : MonoBehaviour {
+        public static GameEvent collectKey;
+        public static GameEvent placePaper2;
         public static GameEvent solveP1;
+        public static GameEvent unlockDoor;
         public static GameEvent seePaper1;
         public static GameEvent seePaper2;
 
+        [SerializeField] private GameEvent _collectKey;
+        [SerializeField] private GameEvent _placePaper2;
         [SerializeField] private GameEvent _solveP1;
+        [SerializeField] private GameEvent _unlockDoor;
         [SerializeField] private GameEvent _seePaper1;
         [SerializeField] private GameEvent _seePaper2;
 
         private void Start() {
+            collectKey = _collectKey;
             solveP1 = _solveP1;
+            unlockDoor = _unlockDoor;
+            placePaper2 = _placePaper2;
             seePaper1 = _seePaper1;
             seePaper2 = _seePaper2;
         }
--- Assets/Scripts/Cardinal/Event/L1.cs
+++ Assets/Scripts/Cardinal/Event/L1.cs
@@ -2,18 +2,34 @@
 
 namespace Event {
     public class L1 : MonoBehaviour {
+        public static GameEvent checkStew;
         public static GameEvent drinkStew;
+        public static GameEvent placeIngredient;
+        public static InventoryItemGameEvent removeIngredient;
+        public static GameEvent seeBook;
         public static GameEvent solveP1;
         public static GameEvent solveP2;
+        public static GameEvent unlockDoor;
 
+        [SerializeField] private GameEvent _checkStew;
         [SerializeField] private  GameEvent _drinkStew;
+        [SerializeField] private GameEvent _placeIngredient;
+        [SerializeField] private InventoryItemGameEvent _removeIngredient;
+        [SerializeField] private  GameEvent _seeBook;
         [SerializeField] private  GameEvent _solveP1;
         [SerializeField] private GameEvent _solveP2;
+        [SerializeField] private  GameEvent _unlockDoor;
 
         private void Start() {
+            placeIngredient = _placeIngredient;
+            removeIngredient = _removeIngredient;
+            solveP2 = _solveP2;
+            seeBook = _seeBook;
             solveP1 = _solveP1;
             solveP2 = _solveP2;
             drinkStew = _drinkStew;
+            unlockDoor = _unlockDoor;
+            checkStew = _checkStew;
         }
     }
 }
