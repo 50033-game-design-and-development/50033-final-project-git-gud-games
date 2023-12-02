80400cbe75a9167d8541834d51a501127a217afb
jaron <jaronho@protonmail.com>
2023-12-02 18:05:25 +0800

    Cleaned up L2

--- Assets/Scripts/Level/Puzzles/L2/FallingPicture.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class FallingPicture : MonoBehaviour {
-    public void Animate(MonologueKey key) {
-        if (key == MonologueKey.L2_PICTURE_LOOSE) {
-            Event.L2.clickPainting.Raise();
-        }
-    }
-}
