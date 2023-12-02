05f8116fa82c56a1521b99c699d5b859ca579cd2
Divy <divy1211.dc@gmail.com>
2023-12-02 15:33:57 +0800

    Fixed main menu load unpause

--- Assets/Scripts/UI/PauseMenu.cs
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -2,7 +2,10 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour {
-    public void OnMainMenu() => SceneManager.LoadSceneAsync("Main Menu", LoadSceneMode.Single);
+    public void OnMainMenu() {
+        SceneManager.LoadScene("Main Menu", LoadSceneMode.Single);
+        GameState.TogglePause();
+    }
 
     public void OnResume() => GameState.TogglePause();
 }
