fb6099db69ae64b2fe86efd4a7b35654c6ddceb0
jaron <jaronho@protonmail.com>
2023-12-06 00:18:28 +0800

    Added glitch effect when shadow despawns

--- Assets/Scripts/Level/Miscellaneous/Shadow.cs
+++ Assets/Scripts/Level/Miscellaneous/Shadow.cs
@@ -6,6 +6,8 @@ public class Shadow : MonoBehaviour {
     [SerializeField] private float despawnDelay;
 
     private IEnumerator Despawn() {
+        GetComponent<CameraGlitch>().Glitch(despawnDelay);
+        GetComponent<AudioSource>().Play();
         yield return new WaitForSeconds(despawnDelay);
         Event.L2.shadowDespawned.Raise();
         Destroy(gameObject);
