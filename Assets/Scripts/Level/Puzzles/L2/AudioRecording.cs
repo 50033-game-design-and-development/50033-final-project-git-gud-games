dca093196521c1f4daacf663f061bf10cc5760eb
jaron <jaronho@protonmail.com>
2023-12-05 11:42:42 +0800

    Made skip monologue work with audio recording

--- Assets/Scripts/Level/Puzzles/L2/AudioRecording.cs
+++ Assets/Scripts/Level/Puzzles/L2/AudioRecording.cs
@@ -1,9 +1,12 @@
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class AudioRecording : MonoBehaviour {
     [SerializeField] private AudioSource audioSource;
     private MonologueKey audioKey = MonologueKey.L2_PC_AUDIO;
+    private PlayerAction _playerAction;
+    private bool inMonologue;
 
     public void StartRecording(MonologueKey monologueKey) {
         if (monologueKey == audioKey) {
@@ -11,6 +14,7 @@ public class AudioRecording : MonoBehaviour {
         }
         else if (monologueKey == MonologueKey.TERMINATE) {
             StopCoroutine("Recording");
+            StopCoroutine("WaitForMonologue");
             audioSource.Stop();
         }
     }
@@ -23,9 +27,40 @@ public class AudioRecording : MonoBehaviour {
             audioSource.PlayOneShot(voiceLines);
 
             // Wait for monologue to be spoken completely
-            yield return new WaitForSeconds(voiceLines.length);
+            inMonologue = true;
+            StartCoroutine("WaitForMonologue", voiceLines.length);
+            while (inMonologue) {
+                yield return null;
+            }
+            StopCoroutine("WaitForMonologue");
+            audioSource.Stop();
         }
 
         Event.Global.endMonologue.Raise(audioKey);
+    }
+
+    private IEnumerator WaitForMonologue(float duration) {
+        yield return new WaitForSeconds(duration);
+        inMonologue = false;
+    }
+
+    private void SkipCurrentText() {
+        inMonologue = false;
+    }
+
+    private void Start() {
+        _playerAction = new PlayerAction();
+        _playerAction.Enable();
+        _playerAction.gameplay.SkipText.performed += _ => SkipCurrentText();
+    }
+
+    private void OnEnable() {
+        if (_playerAction != null) {
+            _playerAction.Enable();
+        }
+    }
+
+    private void OnDisable() {
+        _playerAction.Disable();
     }
 }
